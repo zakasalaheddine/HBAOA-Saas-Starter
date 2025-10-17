0757dbabecc09e5169a3d53fc53eab64898fd715
Salah Eddine Zaka <zakasalaheddine@gmail.com>
2025-10-17 19:27:22 +0100

    feat: add app dev to separate from the serve

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import { Hono } from 'hono'
+import app from "@/app";
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-export default app
+export default {
+	port: 9000,
+	fetch: app.fetch,
+};
